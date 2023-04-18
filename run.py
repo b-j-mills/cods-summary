5e94469c7409eefe3e5e24bec38bfc2620545b37
Briar <briar.j.mills@gmail.com>
2023-04-18 13:58:37 -0400

    Add cowboy COD check and email notif

--- run.py
+++ run.py
@@ -6,9 +6,11 @@ from os.path import expanduser, join
 from hdx.api.configuration import Configuration
 from hdx.facades.keyword_arguments import facade
 from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
 from hdx.utilities.path import temp_dir
 from scripts.check_boundary_fields import check_boundary_fields
 from scripts.check_population_headers import check_population_headers
+from scripts.cowboy_cods import cowboy_cods
 from scripts.metadata_summary import metadata_summary
 
 logger = logging.getLogger(__name__)
@@ -18,6 +20,7 @@ lookup = "cods-summary"
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-hs", "--hdxsite", default=None, help="HDX site")
     parser.add_argument("-sc", "--scrapers", default=None, help="Scrapers to run")
     parser.add_argument("-co", "--countries", default=None, help="Which countries to check")
     args = parser.parse_args()
@@ -30,28 +33,37 @@ def main(
     **ignore,
 ):
     configuration = Configuration.read()
+    with ErrorsOnExit() as errors_on_exit:
+        with temp_dir() as temp_folder:
+            with Download(rate_limit={"calls": 1, "period": 0.1}) as downloader:
+                if scrapers_to_run:
+                    logger.info(f"Running only scrapers: {scrapers_to_run}")
+                if countries:
+                    logger.info(f"Running countries: {countries}")
+                if "metadata_summary" in scrapers_to_run:
+                    metadata_summary(configuration)
+                if "check_population_headers" in scrapers_to_run:
+                    check_population_headers(configuration, downloader, countries)
+                if "check_boundary_fields" in scrapers_to_run:
+                    check_boundary_fields(configuration, countries, temp_folder)
+                if "cowboy_cods" in scrapers_to_run:
+                    cowboy_cods(errors_on_exit)
 
-    with temp_dir() as temp_folder:
-        with Download(rate_limit={"calls": 1, "period": 0.1}) as downloader:
-            if scrapers_to_run:
-                logger.info(f"Running only scrapers: {scrapers_to_run}")
-            if countries:
-                logger.info(f"Running only countries: {countries}")
-            if "metadata_summary" in scrapers_to_run:
-                metadata_summary(configuration)
-            if "check_population_headers" in scrapers_to_run:
-                check_population_headers(configuration, downloader, countries)
-            if "check_boundary_fields" in scrapers_to_run:
-                check_boundary_fields(configuration, countries, temp_folder)
+            if len(errors_on_exit.errors) > 0:
+                with open("errors.txt", "w") as fp:
+                    fp.write("\n".join(errors_on_exit.errors))
 
 
 if __name__ == "__main__":
     args = parse_args()
+    hdx_site = args.hdxsite
+    if not hdx_site:
+        hdx_site = getenv("HDX_SITE", "prod")
     scrapers_to_run = args.scrapers
     if scrapers_to_run is None:
         scrapers_to_run = getenv(
             "SCRAPERS_TO_RUN",
-            "metadata_summary,check_population_headers,check_boundary_fields",
+            "metadata_summary,cowboy_cods",
         )
     if scrapers_to_run:
         scrapers_to_run = scrapers_to_run.split(",")
@@ -64,7 +76,7 @@ if __name__ == "__main__":
         main,
         scrapers_to_run=scrapers_to_run,
         countries=countries,
-        hdx_site="prod",
+        hdx_site=hdx_site,
         hdx_read_only=True,
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yml"),
         user_agent_lookup=lookup,
---
+++ scripts/cowboy_cods.py
@@ -0,0 +1,29 @@
+import logging
+
+from hdx.data.dataset import Dataset
+
+logger = logging.getLogger(__name__)
+
+lookup = "cods-summary"
+
+
+def cowboy_cods(errors_on_exit):
+    logger.info("Finding cowboy CODs")
+
+    datasets = Dataset.search_in_hdx(fq='vocab_Topics:"common operational dataset-cod"')
+
+    if len(datasets) == 0:
+        return
+
+    dataset_names = ",".join([d.get("name") for d in datasets])
+    errors_on_exit.add(f"Found {len(datasets)} dataset(s) with COD tags: {dataset_names}")
+
+    datasets_without_level = [d for d in datasets if not d.get("cod_level")]
+
+    if len(datasets_without_level) == 0:
+        return
+
+    dataset_names = ",".join([d.get("name") for d in datasets_without_level])
+    errors_on_exit.add(f"Found {len(datasets_without_level)} cowboy COD(s): {dataset_names}")
+
+    return
