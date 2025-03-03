2e398d7786f8e264f0d797c4e32099016d4fd032
Briar <briar.j.mills@gmail.com>
2025-03-03 09:05:04 -0500

    Remove ITOS check

--- scripts/metadata_summary.py
+++ scripts/metadata_summary.py
@@ -23,29 +23,12 @@ def metadata_summary(
 
     logger.info(f"Summarizing metadata for {len(datasets)} COD datasets")
 
-    itos_datasets = None
-    itos_names = []
-    try:
-        itos_datasets = get(configuration["itos_url"]).json()
-    except ConnectTimeout:
-        logger.error("Could not connect to ITOS API")
-    if itos_datasets:
-        for i in itos_datasets:
-            location = i["Location"]
-            theme = i["Theme"]
-            if theme == "COD_AB" and (location == ["MMR"] or location == ["mmr"]):
-                name = slugify(i["DatasetTitle"])
-            else:
-                name = slugify(f"{theme} {' '.join(location)}")
-            itos_names.append(name)
-
     results = [[
                 "COD-UID",
                 "dataset title",
                 "URL",
                 "Theme",
                 "Level",
-                "in ITOS API",
                 "number of resource downloads",
                 "source",
                 "contributor/organization",
@@ -70,10 +53,6 @@ def metadata_summary(
         if dataset["name"][:6] in ["cod-ab", "cod-ps", "cod-hp", "cod-em"]:
             theme = dataset["name"][4:6].upper()
 
-        in_itos = "No"
-        if dataset["name"] in itos_names:
-            in_itos = "Yes"
-
         methodology = dataset.get("methodology")
         if methodology == "Other":
             methodology = dataset.get("methodology_other")
@@ -89,7 +68,6 @@ def metadata_summary(
                 dataset.get_hdx_url(),
                 theme,
                 dataset.get("cod_level"),
-                in_itos,
                 dataset.get("total_res_downloads"),
                 dataset.get("dataset_source"),
                 dataset.get_organization()["title"],
